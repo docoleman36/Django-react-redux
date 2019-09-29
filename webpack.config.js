3943e7722f96bc536fc4106cc23cb33bc8dc70af
Donald Coleman <donaldcoleman@Donalds-MacBook-Pro.local>
2019-09-28 17:00:12 -0700

    Working on errors

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+// module.exports = {
+//     module: {
+//       rules: [
+//         {
+//           test: /\.js$/,
+//           exclude: /node_modules/,
+//           use: {
+//             loader: "babel-loader"
+//           }
+//         }
+//       ]
+//     }
+//   }
+
+module.exports = {
+  rules: [
+    {
+      test: /\.js/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    }
+  ]
+}
